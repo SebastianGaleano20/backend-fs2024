2274858e5c2815b4f76f43dbd4e4078aeb622f95
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2024-05-10 23:02:35 -0300

    Add errorHandler middleware for handling errors

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,24 @@
+const ERROR_HANDLERS = {
+
+  sqlError:() => {},
+  gabiError:() => {},
+
+  defaultError: (response, error) => {
+    response
+      .status(500)
+      .json({
+        success: false,
+        message: error.message
+      })
+  }
+}
+
+const errorHandler = (error, _request, response, _next) => {
+  console.error('Error Handler')
+
+  const handler = ERROR_HANDLERS[error.name] || ERROR_HANDLERS.defaultError
+
+  handler(response, error)
+}
+
+export default errorHandler
