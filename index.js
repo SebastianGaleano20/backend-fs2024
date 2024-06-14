d0587033d6d91b33f01cfa4158278920f82f3692
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2024-06-13 23:41:07 -0300

    feat: Add userController with register, login, and profile functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { bookRoutes } from './routes/bookRouter.js'
+import userRouter from './routes/userRouter.js'
 import errorHandler from './middlewares/errorHandler.js'
 
 dotenv.config()
@@ -10,7 +11,7 @@ const SERVER_PORT = process.env.SERVER_PORT || 3001
 const app = express()
 app.use(express.json())
 
-app.use('/api', bookRoutes())
+app.use('/api', bookRoutes(), userRouter)
 app.use(errorHandler)
 
 app.listen(SERVER_PORT, () => {
