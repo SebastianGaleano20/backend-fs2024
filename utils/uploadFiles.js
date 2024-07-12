a91345dc82640d2b2da26e58706f2dfc732b9753
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2024-07-11 21:02:28 -0300

    feat: Add file upload utility function using AWS S3

---
+++ utils/uploadFiles.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import { s3 } from './s3.js'
+
+export const upload = multer({
+  // storage: multer.memoryStorage(),
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname })
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + '-' + file.originalname)
+    },
+  }),
+  limits: { fileSize: 2000000 },
+}).single('file')
